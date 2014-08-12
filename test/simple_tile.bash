4767d8a59e60209a1e5d94f2f2d4569ae93d5219
Ryuichi UEDA <ryuichiueda@gmail.com>
2014-08-12 10:46:28 +0900

    Addition of test

---
+++ test/simple_tile.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+tmp=/tmp/$$
+
+ERROR_CHECK(){
+	[ $(tr -d ' 0' <<< ${PIPESTATUS[@]} ) = "" ] && return
+	echo "ERROR:" $1 >&2
+	rm -f $tmp-*
+	exit 1
+}
+
+#TEST1#####################################
+# test with the simple_tile sample
+
+../samples/simple_tile/execute.bash > $tmp-output
+
+cat << FIN > $tmp-ans
+0 4000
+1 3000
+2 2000
+3 3000
+4 2000
+5 1000
+6 2000
+7 1000
+8 0
+FIN
+
+diff $tmp-output $tmp-ans >&2 || ERROR_CHECK "TEST1"
+
+echo OK >&2
+rm -f $tmp-*
