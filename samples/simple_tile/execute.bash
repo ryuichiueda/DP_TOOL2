3b2bd759f2532f154033b92ec9f154c35a8b6203
Ryuichi UEDA <ryuichiueda@gmail.com>
2014-08-12 10:58:11 +0900

    Writing README for the simple_tile sample

--- samples/simple_tile/execute.bash
+++ samples/simple_tile/execute.bash
@@ -15,4 +15,7 @@ value_iteration $dir/state_trans > $dir/values.3
 cat $dir/values.3			|
 value_iteration $dir/state_trans > $dir/values.4
 
-cat $dir/values.4
+cat $dir/values.4			|
+value_iteration $dir/state_trans > $dir/values.5
+
+cat $dir/values.5
